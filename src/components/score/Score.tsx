977d8339dbc2f43c64a7d796f2dd06b9d2dff287
ekozhaeva <ekozhaeva@evolution.com>
2023-04-09 22:04:27 +0300

    Add score changing on click

--- src/components/score/Score.tsx
+++ src/components/score/Score.tsx
@@ -1,7 +1,17 @@
 import "./Score.css"
+import {useState} from "react";
 
 export const Score = () => {
+    let [score, setScore] = useState(0);
+    function incrementCount(): void {
+        setScore(++score);
+    }
     return (
-        <div className="score-container"> Score </div>
+        <div className="score-container"  onClick={incrementCount}>
+            <div className="number">
+                <h2>Your score is:</h2>
+                <h1>{score}</h1>
+            </div>
+        </div>
     )
 }
